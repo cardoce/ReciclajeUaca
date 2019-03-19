239df1a7835fac43565d4215efeb31b8c969e91b
Fernando Cardoce <fcardoce@pernix-solutions.com>
2019-03-19 14:53:25 -0600

    Add add points feature

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :index 
+      resources :collection_points, only: :index 
     end
   end
 
