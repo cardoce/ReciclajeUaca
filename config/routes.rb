f19b3ac922d0e852b0a08770c54f5e2ce7e30bbf
Fernando Cardoce <fcardoce@pernix-solutions.com>
2019-04-01 16:16:18 -0600

    Add add 10 points to user

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :index 
-      resources :collection_points, only: :index 
+      resources :collection_points, only: :index do
+        collection do
+          get :assign_points
+        end
+      end
     end
   end
 
